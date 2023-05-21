defad68d3cff14005db73d63b056ad7b3c67ab71
Marquis Abah <marquis.abah@shuttlers.ng>
2023-05-21 13:19:40 +0100

    commented out code lines

--- server.js
+++ server.js
@@ -49,9 +49,9 @@ app.use("/api/event", eventRouter);
 app.use("/api/member-purchase", memberPurchaseRouter);
 app.use("/api/news-room", newsRoomRouter);
 
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
@@ -62,9 +62,9 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // // render the error page
+  // res.status(err.status || 500);
+  // res.render('error');
 });
 
 module.exports = app;
